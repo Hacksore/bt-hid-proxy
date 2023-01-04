d136f910e135d31c5a74217976b31de17c3cfe13
mohaque0 <44129090+mohaque0@users.noreply.github.com>
2023-01-04 00:19:20 -0500

    setup.sh copies bt-hid-proxy from the correct directory.

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ set -e
 # Treat undefined environment variables as errors.
 set -u
 
-if ! grep 'dtoverlay=dwc2' /boot/config; then
+if ! grep 'dtoverlay=dwc2' /boot/config.txt; then
   echo "dtoverlay=dwc2" >> /boot/config.txt
 fi
 
@@ -25,7 +25,7 @@ wget https://raw.githubusercontent.com/mtlynch/ansible-role-key-mime-pi/master/f
   -O "$ENABLE_RPI_HID_PATH"
 chmod +x "$ENABLE_RPI_HID_PATH"
 
-cd $(mktemp -d)
+pushd $(mktemp -d)
 wget https://raw.githubusercontent.com/mtlynch/ansible-role-key-mime-pi/master/templates/usb-gadget.systemd.j2
 sed -e "s@{{ key_mime_pi_initialize_hid_script_path }}@${ENABLE_RPI_HID_PATH}@g" \
   usb-gadget.systemd.j2 > /lib/systemd/system/usb-gadget.service
@@ -34,6 +34,7 @@ systemctl daemon-reload
 systemctl enable usb-gadget.service
 
 # install/enable the python service
+popd
 sudo cp ./bt-hid-proxy.service /etc/systemd/system
 systemctl enable bt-hid-proxy.service
 systemctl daemon-reload
